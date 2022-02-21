1d5f03cddd6f648ff0b8f10c163dafe77cc4ccff
harshitaandurkar <100119284+harshitaandurkar@users.noreply.github.com>
2022-02-21 19:19:47 +1100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+
+console.log("hello");
+console.log("Welcome to my first Program");
+console.log("My name is Harshita");
+console.log("I live in Australia");
+console.log("I live in a state victoria");
